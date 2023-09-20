20709378c16c1da96288a563fb9e03395f7857a5
Nurzhan Bakhytuly <satera1583@gmail.com>
2023-09-20 18:38:54 +0600

    deleted

--- lecture2/hw/task2/task2.go
+++ lecture2/hw/task2/task2.go
@@ -8,8 +8,8 @@ type ListNode struct {
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	previous := &ListNode{}
-	current := previous
+	dummy := &ListNode{}
+	current := dummy
 	p1, p2 := list1, list2
 
 	for p1 != nil && p2 != nil {
@@ -23,26 +23,33 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		current = current.Next
 	}
 
+	// If one of the lists becomes empty, append the remaining list
 	if p1 != nil {
 		current.Next = p1
 	} else {
 		current.Next = p2
 	}
 
-	return previous.Next
+	return dummy.Next // Return the next of dummy since dummy itself is an empty placeholder.
 }
 
 func main() {
-
+	// Create list1: 1 -> 2 -> 4
 	list1 := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 4}}}
+
+	// Create list2: 1 -> 3 -> 4
 	list2 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}
+
+	// Merge the two lists
 	mergedList := mergeTwoLists(list1, list2)
+
+	// Print the merged list
 	printList(mergedList)
 }
 
 func printList(node *ListNode) {
 	for node != nil {
-		fmt.Print(node.Val, " ")
+		fmt.Print(node.Val, " -> ")
 		node = node.Next
 	}
 	fmt.Println("nil")
